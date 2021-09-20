81ca774aec095215000d2877ba3a31f933d7986d
Swapnil Tiwari <swaptiwari@outlook.com>
2021-09-20 23:23:43 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
-let square;
-square = 5;
-console.log(square);
-
-let thenumber = 8;
-console.log("Your number is square root of " + thenumber * thenumber);
+function printFarmInventory(cows, chickens) {
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    cowString = "0" + cowString;
+  }
+  console.log(`${cowString} Cows`);
+  let chickenString = String(chickens);
+  while (chickenString.length < 3) {
+    chickenString = "0" + chickenString;
+  }
+  console.log(`${chickenString} Chickens`);
+}
+printFarmInventory(7, 11);
