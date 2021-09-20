0798f6ca7ed0bdb7709083ed757ae87331ba87d8
Swapnil Tiwari <swaptiwari@outlook.com>
2021-09-21 02:22:05 +0530

    Create minmax.js

---
+++ minmax.js
@@ -0,0 +1,26 @@
+function maximum(a, b) {
+  comparison(a, b);
+  console.log(`${max} is the maximum`);
+}
+
+function minimum(a, b) {
+  comparison(a, b);
+  console.log(`${min} is the minimum`);
+}
+
+let min = 0;
+let max = 0;
+function comparison(a, b) {
+  if (a > b) {
+    min = b;
+    max = a;
+  } else if (a < b) {
+    min = a;
+    max = b;
+  } else {
+    min = max;
+  }
+}
+
+console.log(minimum(2, 9));
+console.log(maximum(6, 7));
