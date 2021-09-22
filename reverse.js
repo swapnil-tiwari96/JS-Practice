c1416e11812c58f0f97d7bdceef55b0c932e2570
Swapnil Tiwari <swaptiwari@outlook.com>
2021-09-23 00:06:03 +0530

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,4 +1,4 @@
-array1 = [1, 2, 4, 5, 6, 7];
+array1 = [6, 4, 2, 3, 9, 8];
 function reverseArray(array1) {
   let array2 = [];
   for (let i = array1.length - 1; i >= 0; i--) {
@@ -7,6 +7,16 @@ function reverseArray(array1) {
   return array2;
 }
 
+function reverseSame(array1) {
+  for (i = 0; i < Math.floor(array1.length / 2); i++) {
+    let c = array1[i];
+    array1[i] = array1[array1.length - i - 1];
+    array1[array1.length - i - 1] = c;
+  }
+  return array1;
+}
 console.log(reverseArray(array1));
-console.log(array1.length);
+console.log(reverseSame(array1));
 console.log(array1.length - 1);
+console.log(array1.length);
+console.log(Math.floor(array1.length / 2));
