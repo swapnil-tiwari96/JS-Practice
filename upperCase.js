f4a6a5f5c73f318d44304aa32a8e1b13951ae7d1
Swapnil Tiwari <swaptiwari@outlook.com>
2021-09-26 18:22:07 +0530

    Create upperCase.js

---
+++ upperCase.js
@@ -0,0 +1,22 @@
+// This solution will cap the first letter of the word.
+var nam = ["sWApniL AJay TIWari", "AAyAnsh shah", "AtharV ShARma"];
+let newArray = [];
+
+function overall(nam) {
+  for (let word of nam) {
+    newWord = word.split(" ");
+    let supWord = [];
+    for (let i = 0; i <= newWord.length - 2; i++) {
+      let word = caps(newWord[i]);
+      supWord = supWord + word + " ";
+    }
+    supWord = supWord + caps(newWord[newWord.length - 1]);
+    newArray.push(supWord);
+  }
+  return newArray;
+}
+
+function caps(word) {
+  return word[0].toUpperCase() + word.slice(1, word.length).toLowerCase();
+}
+console.log(overall(nam));
