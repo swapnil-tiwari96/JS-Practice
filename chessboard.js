7ff2e2229356a3d244f9d0a18e20ef70713ac9d8
Swapnil Tiwari <swaptiwari@outlook.com>
2022-06-26 17:13:38 +0530

    changes

--- chessboard.js
+++ chessboard.js
@@ -1,12 +1,42 @@
 let size = 10
 
+//string.repeat() method
 for (let i = 0; i < size; i++)
 {
   if (i % 2 == 0)
   {
-    console.log("# # # #");
+    let str = "# "
+    console.log(str.repeat(size));
   } else
   {
-    console.log(" # # # #");
+    let str = " #"
+    console.log(str.repeat(size));
   }
 }
+
+//for loop method
+
+for (let i = 0; i < size; i++)
+{
+  if (i % 2 == 0)
+  {
+    let str = "# "
+    let multstr = ""
+    for (j = 0; j < size; j++)
+    {
+      multstr += str
+    }
+    console.log(multstr)
+  } else
+  {
+    let str = " #"
+    let multstr = ""
+    for (j = 0; j < size; j++)
+    {
+      multstr += str
+    }
+    console.log(multstr)
+
+  }
+}
+
--- power.js
+++ power.js
@@ -1,13 +1,33 @@
-let counter = 0;
-let result = 1;
-while (counter < 10) {
-  result *= 3;
-  counter += 1;
+let base = 2;
+let exp = 4;
+let resultFor = 1;
+let resultWhile = 1;
+
+//for Loop Version
+for (let i = 0; i < exp; i++)
+{
+  resultFor = resultFor * base;
 }
 
-let result2 = 1;
-for (counter = 0; counter < 10; counter += 1) {
-  result2 *= 2;
+//while loop version
+let counter = 0
+while (counter < exp)
+{
+  resultWhile = resultWhile * base;
+  counter++;
 }
 
-console.log(result, result2);
+//recursion version
+function resultRecursion(base, exp)
+{
+  if (exp == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return base * resultRecursion(base, exp - 1)
+  }
+}
+
+console.log(resultFor, resultWhile, resultRecursion(base, exp))
